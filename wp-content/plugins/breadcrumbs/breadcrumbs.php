8f29c0df571e336e5353835ef1c229f39ca4a5ec
Bohdan <bogmira777@gmail.com>
2023-05-19 22:55:40 +0200

    Fix breadcrumbs plugin

--- wp-content/plugins/breadcrumbs/breadcrumbs.php
+++ wp-content/plugins/breadcrumbs/breadcrumbs.php
@@ -8,7 +8,7 @@ Author: Bohdan Shcherbak
 Author URI: https://example.com/
 License: GPL2
 */
-if ( !function_exists( 'add_action' ) ) {
+if (!function_exists('add_action')) {
     echo "Hi there! I'm just a plugin, no much I can do when called directly.";
     exit;
 }
@@ -21,9 +21,17 @@ function breadcrumbs()
         echo get_option('home');
         echo '">' . __("Home") . '</a></li>';
         if (is_category() || is_single()) {
-            // the_category(', ');
             ?>
-            <li><a href="<?php echo get_permalink(get_option('page_for_posts')); ?>"><?php echo get_the_title( get_option('page_for_posts', true) ); ?></a></li>
+            <li>
+                <a href="<?php echo get_permalink(get_option('page_for_posts')); ?>"><?php echo get_the_title(get_option('page_for_posts', true)); ?></a>
+            </li>
+            <?php if (is_category()) {
+                $category = get_queried_object();
+                if ($category instanceof WP_Term) {
+                    $category_name = $category->name;
+                    _e("<li>$category_name</li>");
+                }
+            } ?>
             <?php
             if (is_single()) {
                 echo '<li class="active">';
@@ -34,13 +42,14 @@ function breadcrumbs()
             echo '<li class="active">';
             echo the_title();
             echo '</li>';
-        }
-        elseif (is_home()){
+        } elseif (is_home()) {
             ?>
-            <li><?php echo get_the_title( get_option('page_for_posts', true) ); ?></li>
+            <li>
+                <?php echo get_the_title(get_option('page_for_posts', true)); ?>
+            </li>
             <?php
         }
         echo '</ol></div></section>';
-    } 
+    }
 }
 add_action('wp_head', 'breadcrumbs');
