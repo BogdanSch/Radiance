2f8b3841b3c8858eca8eae8cbc91fa318340951c
Bohdan <bogmira777@gmail.com>
2023-06-03 23:24:25 +0200

    Add content-none file, footer improves

--- wp-content/themes/radiance/footer.php
+++ wp-content/themes/radiance/footer.php
@@ -23,10 +23,6 @@
                             }
                         }
                         ?>
-                        <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a> -->
                     </div>
 
 
