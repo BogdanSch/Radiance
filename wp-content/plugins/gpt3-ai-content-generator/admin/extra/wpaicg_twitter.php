3930394a181f154f39e687c5a3684bf766ddc997
Bohdan <bogmira777@gmail.com>
2023-06-26 22:25:53 +0200

    Update commit

--- wp-content/plugins/gpt3-ai-content-generator/admin/extra/wpaicg_twitter.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-?>
-<a href="<?php echo esc_url(admin_url('admin.php?page=wpaicg-pricing'))?>"><img src="<?php echo esc_url(WPAICG_PLUGIN_URL)?>admin/images/twitter.png" width="70%"></a>
