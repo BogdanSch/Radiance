bda2cce21ab6604bb5e4754de9760a1ae8769669
Bohdan <bogmira777@gmail.com>
2023-07-09 22:30:27 +0200

    Bs slider fixes

--- wp-content/plugins/bs-slider/includes/front/enqueue.php
+++ wp-content/plugins/bs-slider/includes/front/enqueue.php
@@ -4,12 +4,10 @@ function kc_enqueue_scripts()
     $version = BOOTSTRAPTOPIC_DEV_MODE ? time() : false;
     wp_register_style('bs_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', [], $version);
     wp_register_style('bs_bootslider', plugins_url('/assets/css/slider.css', KC_PLUGIN_URL), [], $version);
-    wp_register_script('bs_bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js', [], $version);
     wp_register_script('bs_bootstrap-js-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', [], $version);
     //Enqueue
     wp_enqueue_style('bs_bootstrap');
     wp_enqueue_style('bs_bootslider');
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bs_bootstrap-js');
     wp_enqueue_script('bs_bootstrap-js-bundle');
 }
